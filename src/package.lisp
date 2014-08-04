6c38b310de785501c39f912b886bf34a28b85010
Torbjørn Marø <torbjorn.maro@gmail.com>
2014-08-04 12:28:21 +0200

    Misc, lot's of TODOs

--- src/package.lisp
+++ src/package.lisp
@@ -9,6 +9,8 @@
   
    ;;
    *user-agent*
+   *aws-host*
+   *rackspace-host*  ; TODO: Add other hosts (http://dev.iron.io/mq/reference/api/#base_url)
 
    ;;
    :make-client
@@ -17,13 +19,21 @@
    :request
    
    ;; QUERIES
+
    :queues
    :queue-size
-   :get-messages
+   :get-messages  ; TODO: optional delete, wait, timeout
    :get-message
+   ; TODO :get-message-by-id
 
    ;; COMMANDS
+
+   ; TODO :delete-queue
+   ; TODO :clear-queue
    :post-messages
    :post-message
+   ; TODO :delete-messages
    :delete-message
+   ; TODO :touch-message
+   ; TODO :release-message
    ))
